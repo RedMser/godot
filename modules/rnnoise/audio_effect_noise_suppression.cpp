dc26ede90c9d3269c576ab11758699f8ab1fb469
RedMser <redmser.jj2@gmail.com>
2022-10-03 19:27:09 +0200

    RNNoise basics
    - needs AudioServer::buffer_size changed to 480
    - lib file is manually compiled and placed into the build folder

---
+++ modules/rnnoise/audio_effect_noise_suppression.cpp
@@ -0,0 +1,87 @@
+/*************************************************************************/
+/*  audio_effect_noise_suppression.cpp                                   */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "audio_effect_noise_suppression.h"
+
+// Current version of rnnoise does not support any other frame size.
+const int FRAME_SIZE = 480;
+
+AudioEffectNoiseSuppressionInstance::AudioEffectNoiseSuppressionInstance() {
+	rnnoise = rnnoise_create(nullptr);
+}
+
+AudioEffectNoiseSuppressionInstance::~AudioEffectNoiseSuppressionInstance() {
+	rnnoise_destroy(rnnoise);
+	rnnoise = nullptr;
+}
+
+void AudioEffectNoiseSuppressionInstance::process(const AudioFrame *p_src_frames, AudioFrame *p_dst_frames, int p_frame_count) {
+	// At the time of writing this code, RNNoise only supports a sample rate of 48000 Hz.
+	// TODO: Check "mix rate" project setting and warn accordingly.
+
+	if (p_frame_count < FRAME_SIZE) {
+		WARN_PRINT_ONCE(vformat("Can't use RNNoise, because AudioServer's buffer size (%d) is set below the minimum required FRAME_SIZE of %d", p_frame_count, FRAME_SIZE));
+		return;
+	}
+
+	// TODO: Pack left and right channels next to each other.
+	// TODO: delay by 1 block, so we always can fill 512 samples using the 480 process_frame
+	PackedFloat32Array denoise_frames;
+	denoise_frames.resize(p_frame_count * 2);
+	float *denoise_frame_ptr = denoise_frames.ptrw();
+	for (int i = 0; i < p_frame_count; i++) {
+		short quantized_l = p_src_frames[i].l * static_cast<float>(std::numeric_limits<short>::max());
+		short quantized_r = p_src_frames[i].r * static_cast<float>(std::numeric_limits<short>::max());
+		denoise_frame_ptr[i] = quantized_l;
+		denoise_frame_ptr[i + p_frame_count] = quantized_r;
+	}
+
+	// TODO: Allow reading return value (voice activity detection probability)
+	rnnoise_process_frame(rnnoise, denoise_frame_ptr, denoise_frame_ptr);
+
+	for (int i = 0; i < p_frame_count; i++) {
+		p_dst_frames[i].l = denoise_frame_ptr[i] / static_cast<float>(std::numeric_limits<short>::max());
+		p_dst_frames[i].r = denoise_frame_ptr[i + p_frame_count] / static_cast<float>(std::numeric_limits<short>::max());
+	}
+}
+
+Ref<AudioEffectInstance> AudioEffectNoiseSuppression::instantiate() {
+	Ref<AudioEffectNoiseSuppressionInstance> ins;
+	ins.instantiate();
+	ins->base = Ref<AudioEffectNoiseSuppression>(this);
+
+	return ins;
+}
+
+void AudioEffectNoiseSuppression::_bind_methods() {
+}
+
+AudioEffectNoiseSuppression::AudioEffectNoiseSuppression() {
+}
---
+++ modules/rnnoise/audio_effect_noise_suppression.h
@@ -0,0 +1,66 @@
+/*************************************************************************/
+/*  audio_effect_noise_suppression.h                                     */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef AUDIO_EFFECT_NOISE_SUPPRESSION_H
+#define AUDIO_EFFECT_NOISE_SUPPRESSION_H
+
+#include "thirdparty/rnnoise/rnnoise.h"
+#include "servers/audio/audio_effect.h"
+
+class AudioEffectNoiseSuppression;
+
+class AudioEffectNoiseSuppressionInstance : public AudioEffectInstance {
+	GDCLASS(AudioEffectNoiseSuppressionInstance, AudioEffectInstance);
+	friend class AudioEffectNoiseSuppression;
+	Ref<AudioEffectNoiseSuppression> base;
+
+	DenoiseState *rnnoise;
+
+public:
+	virtual void process(const AudioFrame *p_src_frames, AudioFrame *p_dst_frames, int p_frame_count) override;
+
+	AudioEffectNoiseSuppressionInstance();
+	~AudioEffectNoiseSuppressionInstance();
+};
+
+class AudioEffectNoiseSuppression : public AudioEffect {
+	GDCLASS(AudioEffectNoiseSuppression, AudioEffect);
+	friend class AudioEffectNoiseSuppressionInstance;
+
+protected:
+	static void _bind_methods();
+
+public:
+	Ref<AudioEffectInstance> instantiate() override;
+
+	AudioEffectNoiseSuppression();
+};
+
+#endif // AUDIO_EFFECT_NOISE_SUPPRESSION_H
